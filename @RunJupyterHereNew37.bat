dded36ac723119fae7228948807e95d37f93a2c8
Mohammad Elnesr <drnesr@gmail.com>
2018-12-24 14:45:47 +0300

    Reforming the dataset to 8 columns

---
+++ @RunJupyterHereNew37.bat
@@ -0,0 +1,13 @@
+REM # A batch file to...
+REM # Run Jupyter notebook  on the current folder 
+setlocal
+cd /d %~dp0
+
+REM # If you need special Anaconda environment
+REM # Uncomment the following line
+REM # call activate py3
+
+REM # It is important to add the 'call' 
+call jupyter notebook
+REM # The pause command is to keep the command window active.
+pause
